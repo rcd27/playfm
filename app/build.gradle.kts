1205dbbefad9491df7a3703e9944d14498ae46fb
Stanislav Zemlyakov <redtom@yandex.ru>
2020-01-27 04:49:27 +0300

    Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
                         val output = t as BaseVariantOutputImpl
                         val variantName = variant.buildType.name
                         output.outputFileName =
-                            "Pikabu_test-case$variantName-v${defaultConfig.versionName}.apk"
+                            "Playfm-$variantName-v${defaultConfig.versionName}.apk"
                     }
                 })
             }
@@ -90,13 +90,13 @@ android {
     }
 
     dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
 
         // UI
-        implementation("com.google.android.material:material:1.2.0-alpha02")
+        implementation("com.google.android.material:material:1.2.0-alpha03")
         implementation("androidx.appcompat:appcompat:1.1.0")
         implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-        implementation("androidx.fragment:fragment:1.2.0-rc03")
+        implementation("androidx.fragment:fragment:1.2.0-rc05")
 
         // RecyclerView extensions
         implementation("com.hannesdorfmann:adapterdelegates4:4.2.0")
@@ -104,10 +104,10 @@ android {
         implementation("com.hannesdorfmann:adapterdelegates4-kotlin-dsl-layoutcontainer:4.2.0")
 
         // Network
-        implementation("com.squareup.retrofit2:retrofit:2.6.2")
-        implementation("com.squareup.retrofit2:converter-gson:2.6.0")
+        implementation("com.squareup.retrofit2:retrofit:2.7.1")
+        implementation("com.squareup.retrofit2:converter-gson:2.7.0")
         implementation("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
-        implementation("com.squareup.okhttp3:logging-interceptor:4.0.0")
+        implementation("com.squareup.okhttp3:logging-interceptor:4.3.0")
 
         // Dependency Injection
         implementation("com.google.dagger:dagger:2.25.2")
@@ -122,8 +122,8 @@ android {
         implementation("com.jakewharton.rxrelay2:rxrelay:2.1.0")
 
         // Navigation
-        implementation("androidx.navigation:navigation-fragment:2.2.0-rc03")
-        implementation("androidx.navigation:navigation-ui:2.2.0-rc03")
+        implementation("androidx.navigation:navigation-fragment:2.2.0-rc04")
+        implementation("androidx.navigation:navigation-ui:2.2.0-rc04")
 
         // Load images
         implementation("com.squareup.picasso:picasso:2.71828")
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
         maven { setUrl("https://plugins.gradle.org/m2/")}
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        classpath("com.android.tools.build:gradle:3.5.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
         classpath ("org.jlleitschuh.gradle:ktlint-gradle:8.2.0")
     }
 }
