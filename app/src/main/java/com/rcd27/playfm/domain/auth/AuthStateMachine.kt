a0826bc4ed80a4094e303261111163ba1ccea484
Stanislav Zemlyakov <redtom@yandex.ru>
2020-01-27 03:39:49 +0300

    Fix DI for `AuthStateMachine`

--- app/src/main/java/com/rcd27/playfm/domain/auth/AuthStateMachine.kt
+++ app/src/main/java/com/rcd27/playfm/domain/auth/AuthStateMachine.kt
@@ -1,8 +1,9 @@
 package com.rcd27.playfm.domain.auth
 
 import com.jakewharton.rxrelay2.BehaviorRelay
+import javax.inject.Inject
 
-class AuthStateMachine {
+class AuthStateMachine @Inject constructor() {
 
     val state: BehaviorRelay<AuthState> = BehaviorRelay.create()
 
